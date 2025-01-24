dae5339e15c4b3567c4a24793babfcbe6b7d22d0
Bogdan <Bogdan_070509@mail.ru>
2025-01-24 21:11:42 +0300

    add new function which trasform list in string

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def get_word(words):
     return words[0]
 
+def list_to_string_convert(strings):
+    strr = ""
+    for i in strings:
+        strr += str(i)
+        strr += ""
+    return strr
+
 def start_template(word):
     return len(word) * ["_"]
 
@@ -9,7 +16,9 @@ def game():
     word = ["orange"]
     lifes = 3
     word_in_play = get_word(word)
-    template = start_template(word_in_play)
+    template =start_template(word_in_play)
+    a = list_to_string_convert(template)
+    print(a)
 
 game()
 print("Добро пожаловать в игру Hangman")
