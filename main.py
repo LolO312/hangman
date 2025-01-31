d1d9bccd8080bab277d38d400b34a64b8440c54f
Bogdan <Bogdan_070509@mail.ru>
2025-01-31 18:35:52 +0300

    Add build_template function

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ def get_word(words):
     return words[0]
 
 def list_to_string_convert(strings):
-    strr = ""
-    for i in strings:
-        strr += str(i)
-        strr += ""
-    return strr
+    # string = ""
+    # for i in strings:
+    #     string += i
+    # return string
+    return ''.join(strings)
 
 def start_template(word):
     return len(word) * ["_"]
+
+def build_template(symbols, word, letter):
+    for i in range(len(word)):
+        if word[i] == letter:
+            symbols[i] = letter
+    return symbols
 
 def game():
     progress = True
@@ -25,6 +31,24 @@ def game():
     template = start_template(word_in_play)
     a = list_to_string_convert(template)
     welcome_speech(list_to_string_convert(template))
+
+    while progress:
+        user_guess = input("Введите букву: ")
+        template = build_template(template, word_in_play, user_guess)
+        guessed = list_to_string_convert(template)
+        print(f"Результат: {guessed}")
+        # progress = check_win(guessed)
+        #
+        # if not check_mistake(word_in_play, user_guess):
+        #     print(f"Осталось {lifes} попытки")
+        #     lifes = check_attempt(lifes)
+        #
+        # if lifes == 0:
+        #     lose_speech()
+        #     break
+        #
+        # if not progress:
+        #     win_speach()
 
 game()
 
